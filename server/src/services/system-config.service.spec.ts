2d73e7357963310eca5a2d87594b377473fbbc0c
ZHEQIUSHUI <qiushui.zhe@gmail.com>
2025-08-22 17:04:21 +0800

    默认使用 ViT-L-14-336__axera 注释掉 immich-machine-learning 不拉去镜像

--- server/src/services/system-config.service.spec.ts
+++ server/src/services/system-config.service.spec.ts
@@ -113,7 +113,7 @@ const updatedConfig = Object.freeze<SystemConfig>({
     },
     clip: {
       enabled: true,
-      modelName: 'ViT-B-32__openai',
+      modelName: 'ViT-L-14-336__axera',
     },
     duplicateDetection: {
       enabled: true,
